0cf94e607a84febc0bedc62ca6125c7280d990b6
pujiastutik1 <141117279+pujiastutik1@users.noreply.github.com>
2023-11-10 09:07:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Haloo</title>
+    <title>Sutik</title>
 </head>
 <body>
     
