fbd6af615f83142b205aa696b2b1a56131eb418e
pujiastutik1 <141117279+pujiastutik1@users.noreply.github.com>
2023-11-10 09:21:04 +0700

    sutik

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sutik</title>
+    <title>Sutik ayange mas apa</title>
 </head>
 <body>
     
