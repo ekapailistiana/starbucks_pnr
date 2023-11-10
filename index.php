de3e4bd0833b7b0e44bab4a3179aba1e3194cc61
pujiastutik1 <141117279+pujiastutik1@users.noreply.github.com>
2023-11-10 10:04:46 +0700

    update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sutik ayange mas apa</title>
+    <title>rasya galau</title>
 </head>
 <body>
     <h2>RASYA LAGI GALAU</h2>
