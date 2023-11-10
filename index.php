d2c846850565bd6d9a411ba6e1dc33e651ef0e1b
rasyasasikirana <141130044+rasyasasikirana@users.noreply.github.com>
2023-11-10 10:43:25 +0700

    index fix
    ya pokoknya gitu

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>rasya galau</title>
+	<title>Membuat Login Multi User Level Dengan PHP dan MySQLi - www.malasngoding.com</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h2>RASYA LAGI GALAU</h2>
-    <h2>TIKA LAGI KIPASAN</h2>
-    <h2>JUPI LAGI BAHAGIA</h2>
+ 
+	<h1>Membuat Login Multi User Level Dengan PHP dan MySQLi <br/> www.malasngoding.com</h1>
+ 
+	<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan']=="gagal"){
+			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+		}
+	}
+	?>
+ 
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+ 
+		<form action="cek_login.php" method="post">
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
+ 
+			<label>Password</label>
+			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="https://www.malasngoding.com">kembali</a>
+			</center>
+		</form>
+		
+	</div>
+ 
+ 
 </body>
 </html>
