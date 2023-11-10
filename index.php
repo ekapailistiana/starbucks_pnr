e4608f9b68dea42642ac4dedd183399f48b61bb6
nugiartika <141117388+nugiartika@users.noreply.github.com>
2023-11-10 09:12:25 +0700

    ijup

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sutik</title>
+    <title>ijup bucin</title>
 </head>
 <body>
     
